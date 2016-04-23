cd00dbcdad3b12d6ab628db79a8b25bddcea3f4b
daniel <caetanods1@gmail.com>
2016-04-23 11:09:20 -0700

    Implements phylm to use three.point.compute.

--- R/singleR.loglik.R
+++ R/singleR.loglik.R
@@ -3,33 +3,44 @@
 ##' This function applies a math shortcut to avoid inverting the kronecker product of matrices. The trick is based on an equality described in Ho and Ané (2014). A similar equality also make the calculation of the determinant of the kronecker product faster. Thus, one need to provide the inverse of the phylogenetic covariance matrix as the argument 'C.prime' and the determinant of this same matrix as the argument 'det.C'.
 ##' @title Log likelihood for one evolutionary rate matrix fitted to the data and tree.
 ##' @param X matrix. The matrix of trait data.
+##' @param phy ape phylo. The phylogenetic tree with branch lengths.
 ##' @param root numeric. The phylogenetic mean.
 ##' @param R matrix. The covariance matrix fitted to the tree and data.
-##' @param C.prime matrix. The inverse of the phylogenetic covariance matrix. This is a fixed term.
-##' @param det.C numeric. The determinant (log scale) of the phylogenetic covariance matrix. This is a fixed term.
-##' @param D matrix. The design matrix. This is a fixed term, dependent on the phylogeny.
 ##' @param n numeric. Number of tips in the phylogeny.
 ##' @param r numeric. Number of traits, the dimension of the R matrix.
-##' @param method string. Method to be used to calculate the likelihood. Right now only the option 'rpf' is working. More options will be implemented in the future.
 ##' @return numeric. The log likelhood.
 ##' @references Revell, L. J., and D. C. Collar. 2009. Phylogenetic Analysis of the Evolutionary Correlation Using Likelihood. Evolution 63:1090–1100.
 ##' @references Ho, L. si T., and C. Ané. 2014. A Linear-Time Algorithm for Gaussian and Non-Gaussian Trait Evolution Models. Syst Biol 63:397–408.
-singleR.loglik <- function(X, root, R, C.prime, det.C, D, n, r){
-
-    ## (y - Da)
-    Xvec <- as.vector( as.matrix(X) )
-    residuals <- D %*% root - Xvec
-    ## solve( R ) -> Use the Cholesky decomposition to compute the inverse. This works with square positive-definite matrices. Should be twice as fast as the general solve.
-    R.prime <- chol2inv( chol( R ) )
-    ## solve( V ) -> This is the inverse of the kronecker product.
-    V.prime <- kronecker(R.prime, C.prime)
-    ## det( V ) -> This is the determinant of the kronecker product. NOT in log scale.
-    ## det.V <- kronecker( (det(R))^n, ( det.C )^r )
-    det.V <- ( determinant(R, logarithm=TRUE)$modulus[1] )^n * ( det.C )^r
-    ## The log.lik function:
-    ## Guide code:
-    ## - ((t(y - b) %*% solve(V) %*% (y - b))/2) - determinant(V)$modulus[1]/2 - (n*r*log(2*pi)/2)
-    logl <- -0.5 * ( t(residuals) %*% V.prime %*% residuals) -0.5 * det.V -0.5 * (r * n * log(2 * pi) )
-    
+##' @export
+singleR.loglik <- function(X, phy, root, R, r, n){
+    ## X.c are the residuals. We can think of the root as the intercept of a linear regression.
+    ## This standardize the data. So we can apply the 'three.point.compute'.
+    X.c <- sapply(1:ncol(X), function(x) X[,x] - root[x])
+    P <- data.frame(X.c)
+    ## Need to make sure that tree and data matches.
+    rownames(P) <- phy$tip.label
+    comp <- three.point.compute(phy, P=P, Q=NULL)
+    xiVx <- sum( comp$PP * chol2inv(chol(R)) )
+    detV <- r * comp$logd + n * determinant(R)$modulus[1]
+    logl <- -0.5 * ( r * n * log(2 * pi) + detV + xiVx )
     return(logl)
 }
+
+## Old implementation of the same function:
+## singleR.loglik <- function(X, root, R, C.prime, det.C, D, n, r){
+##     ## (y - Da)
+##     Xvec <- as.vector( as.matrix(X) )
+##     residuals <- D %*% root - Xvec
+##     ## solve( R ) -> Use the Cholesky decomposition to compute the inverse. This works with square positive-definite matrices. Should be twice as fast as the general solve.
+##     R.prime <- chol2inv( chol( R ) )
+##     ## solve( V ) -> This is the inverse of the kronecker product.
+##     V.prime <- kronecker(R.prime, C.prime)
+##     ## det.C is in log scale. So we need to transform all the expression to the log.
+##     ## This is log( ( det(R) )^n * ( det(C) )^r )
+##     det.V <- ( determinant(R)$modulus[1] ) * n + ( det.C ) * r
+##     ## The log.lik function:
+##     ## Guide code:
+##     ## - ((t(y - b) %*% solve(V) %*% (y - b))/2) - determinant(V)$modulus[1]/2 - (n*r*log(2*pi)/2)
+##     logl <- -0.5 * ( ( t(residuals) %*% V.prime %*% residuals) + det.V + (r * n * log(2 * pi) ) )
+##     return(logl)
+## }
