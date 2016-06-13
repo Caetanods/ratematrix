a84d739caca84f43bebb83548c7683cf6ca22ec7
daniel <caetanods1@gmail.com>
2016-06-13 15:48:31 -0700

    Make plot to standard device

--- R/testRateMatrix.R
+++ R/testRateMatrix.R
@@ -12,10 +12,10 @@
 ##' @param mcmc.chain The MCMC chain produced by the read function.
 ##' @param out The output of the MCMC chain. This is a list with a series of important informations about the MCMC chain.
 ##' @param plot logical. Whether plots should be saved as .pdf files in the current directory. The number of the plot increases in function of the number of matrices fitted to the tree. All plots will have a pattern like "post_diff_mat_test_X.pdf", where X is a single digit number.
-##' @param file string. The name of the file to save the plot. This is going to be used as a the start of the string. The number of plots is equal to the combination 2 by 2 of the number of the matrices fitted to the phylogentic tree.
+##' @param file string. The name of the file to save the plot. Default is 'NULL' and will plot in the R standard plotting device. If a string is passed to it, then it will plot to a '.pdf' file. This is going to be used as a the start of the string. The number of plots is equal to the combination 2 by 2 of the number of the matrices fitted to the phylogentic tree.
 ##' @return Return a matrix with the value of the test statistics. Values bellow 0.05 supports a difference between the posterior distribution of R matrices.
 ##' @export
-testRateMatrix <- function(mcmc.chain, out, plot = FALSE, file = "plot_test"){
+testRateMatrix <- function(mcmc.chain, out, plot = TRUE, file = NULL){
     comb <- combn(1:out$p, 2)
     mean.diff <- list()
     for(i in ncol(comb)){
@@ -28,15 +28,24 @@ testRateMatrix <- function(mcmc.chain, out, plot = FALSE, file = "plot_test"){
     qq.list <- lapply(cdf.list, FUN = function(x) x(0) )
     test <- lapply(qq.list, FUN = function(x) 2*apply(cbind(x, 1-x), 1, min) )
     if(plot == TRUE){
-        for(i in ncol(comb)){
-            pdf( paste(file, i, ".pdf", sep="") )
+        if( is.null(file) ){
             hist(mean.diff[[i]], main = paste("mat #",comb[1,i]," vs. mat #",comb[2,i]," test value ="
-                                            , round(test[[i]], digits = 3))
-               , freq = FALSE, xlab = "Posterior mean difference between matrices"
+                                              , round(test[[i]], digits = 3))
+                   , freq = FALSE, xlab = "Posterior mean difference between matrices"
                , col = "grey", border = "white")
             abline(v = 0, col = "red", lty = 3, lwd = 3)
             lines( density(mean.diff[[i]]) )
-            dev.off()
+        } else {
+            for(i in ncol(comb)){
+                pdf( paste(file, i, ".pdf", sep="") )
+                hist(mean.diff[[i]], main = paste("mat #",comb[1,i]," vs. mat #",comb[2,i]," test value ="
+                                                , round(test[[i]], digits = 3))
+                   , freq = FALSE, xlab = "Posterior mean difference between matrices"
+                   , col = "grey", border = "white")
+                abline(v = 0, col = "red", lty = 3, lwd = 3)
+                lines( density(mean.diff[[i]]) )
+                dev.off()
+            }
         }
     }
     test.dt <- do.call(cbind, test)
