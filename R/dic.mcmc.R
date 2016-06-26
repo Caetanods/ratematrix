e1de12952f4b7fd7105fe68301639dccae54d5f6
daniel <caetanods1@gmail.com>
2016-06-26 10:30:30 -0700

    Improves function. Calculation still wrong.

--- R/dic.mcmc.R
+++ R/dic.mcmc.R
@@ -3,40 +3,41 @@
 ##' Function works with the samples from the R matrix MCMC estimate functions.
 ##' \cr
 ##' \cr
-##' Important issue: DIC is prone to missbehave and reject the model with two or more matrixes even when there is clearly a better estimate of the parameters. The posterior distribution for the R matrix is not normally distributed and this might be the reason why the DIC is not a good statistics to test the models comparing different numbers of R matrices. We recommend caution while using DIC.
+##' Important note: DIC uses the mean of the posterior as a summary statistics for the test. If the posterior of parameter estimates is not well summarized by the mean, then the results of the DIC test will have no meaning.
 ##' @title Deviance Information Criteria
 ##' @param out The object with the MCMC info. The return from the MCMC functions.
 ##' @param post The posterior chain read by the 'read.multi.R.iwish' or 'read.single.R.iwish'.
 ##' @return numeric. Return the DIC value.
 ##' @seealso \code{\link{multi.R.iwish.mcmc.R single.R.iwish.mcmc.R}}
 ##' @references Gelman, A., J. Hwang, and A. Vehtari. 2013. Understanding predictive information criteria for Bayesian models. Stat Comput. 24:997–1016.
+##' @importFrom ape vcv.phylo as.phylo
+##' @importFrom phytools multiC
+##' @importFrom corpcor make.positive.definite
 ##' @export
 dic.mcmc <- function(out, post){
-
-    ## Get additional objects to calculate the likelihood.
-    ## See first lines of 'single.R.iwish.mcmc' for what this is doing.    
-    C <- vcv(out$phy)
-    X <- out$data[rownames(C),]
+    cache.data <- list()
+    cache.data$C <- vcv.phylo(out$phy)
+    cache.data$X <- out$data[rownames(cache.data$C),]
+    cache.data$n <- length(out$phy$tip.label)
+    cache.data$r <- out$k
+    D <- matrix(0, nrow = cache.data$n * cache.data$r, ncol = cache.data$r)
+    for(i in 1:cache.data$r) D[((cache.data$n*(i-1))+1):(cache.data$n*i),i] <- 1
+    cache.data$D <- D
     traits <- colnames(out$data)
     y <- matrix( c( as.matrix(out$data) ) )
-    n <- length(out$phy$tip.label)
-    r <- out$k
     root.post <- post$root
     ss <- dim(root.post)[1]
-    b.post <- lapply(1:length(ss), function(y) matrix( sapply(as.vector(root.post[y,]), function(x) rep(x, n) ) ) )
-    D <- matrix(0, nrow = n*r, ncol = r)
-    for(i in 1:r) D[((n*(i-1))+1):(n*i),i] <- 1
 
     ## Check the number of matrices fitted in the model.
     if(out$p > 1){
 
         ## Get the list of C matrices for the multiR.loglik function:
-        C.m <- multiC(out$phy) ## phy vcv matrix for multiple R matrices.
-        names(C.m) <- colnames(out$phy$mapped.edge) ## Give names to each matrix.
+        cache.data$C.m <- multiC(out$phy) ## phy vcv matrix for multiple R matrices.
+        names(cache.data$C.m) <- colnames(out$phy$mapped.edge) ## Give names to each matrix.
         
         ## Use the multiR.loglik
         ## Number of matrices in the model:
-        p <- out$p
+        cache.data$p <- out$p
         
         ## Get the distribution of parameter estimates from the posterior:
         R.post <- post$matrix
@@ -44,13 +45,15 @@ dic.mcmc <- function(out, post){
         ## Calculates the mean parameter estimates from the posterior.
         ## This will be used to calculate the first term of equation (7).
         R.mean <- list()
-        for(i in 1:p){
-            R.mean[[i]] <- Reduce('+', R.post[[i]])/length(R.post[[i]])
+        for(i in 1:cache.data$p){
+            RR <- Reduce('+', R.post[[i]])/length(R.post[[i]])
+            if( det(RR) == 0 ) RR <- make.positive.definite(RR)
+            R.mean[[i]] <- RR
         }
+        
         root.mean <- colMeans(root.post)
-        b.mean <- matrix( sapply(as.vector(root.mean), function(x) rep(x, n) ) )
-        ## lik.mean.par <- multiR.loglik(R.m=R.mean, C.m=C.m, y=y, b=b.mean, n=n, r=r, p=p)
-        lik.mean.par <- multiR.loglik(X=X, root=root.mean, R.m=R.mean, C.m=C.m, D=D, n=n, r=r, p=p)
+        ## Now the likelihood function depends on the caches. So need to update this.
+        lik.mean.par <- multiR.loglik(data=cache.data, chain=NULL, root=root.mean, R=R.mean)
         
         ## Calculates the mean likelihood across the posterior of parameter estimates.
         ## This is already in the output of the MCMC functions.
@@ -71,10 +74,14 @@ dic.mcmc <- function(out, post){
         ## Calculates the mean parameter estimates from the posterior.
         ## This will be used to calculate the first term of equation (7).
         R.mean <- Reduce('+', R.post)/length(R.post)
+
+        ## Need to check if the matrix from the mean parameter estimates is positive-definite.
+        ## if not, generate the nearest positive-definite matrix.
+        if( det(R.mean) == 0 ) R.mean <- make.positive.definite(R.mean)
+
         root.mean <- colMeans(root.post)
-        b.mean <- matrix( sapply(as.vector(root.mean), function(x) rep(x, n) ) )
-        ## lik.mean.par <- singleR.loglik(R=R.mean, C=C, y=y, b=b.mean, n=n, r=r)
-        lik.mean.par <- singleR.loglik(X=X, phy=out$phy, root=root.mean, R=R.mean, r=r, n=n)
+        cache.data$phy <- as.phylo(out$phy)
+        lik.mean.par <- singleR.loglik(data=cache.data, chain=NULL, root=root.mean, R=R.mean)
         
         ## Calculates the mean likelihood across the posterior of parameter estimates.
         ## This will be the second term of equation (8).
