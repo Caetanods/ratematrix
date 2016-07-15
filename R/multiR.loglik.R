bdccce98f829c24664274dc2ffcb4aa1871bc212
daniel <caetanods1@gmail.com>
2016-07-15 11:38:29 -0700

    Make snapshot from previous version. Unclear what changed in this version.

--- R/multiR.loglik.R
+++ R/multiR.loglik.R
@@ -9,6 +9,7 @@
 ##' @return numeric. The log likelhood.
 ##' @references Revell, L. J., and D. C. Collar. 2009. Phylogenetic Analysis of the Evolutionary Correlation Using Likelihood. Evolution 63:1090–1100.
 multiR.loglik <- function(data, chain, root, R){
+    ## Try to keep the same parameters for the likelihood function. Make it easier for the MCMC to run as it is now.
     ## Default method is "rpf"
     C.m <- data$C.m
     X <- data$X
@@ -37,3 +38,35 @@ multiR.loglik <- function(data, chain, root, R){
     logl <- -.5*quad-.5*as.numeric(det)-.5*(ntot*log(2*pi))
     return(logl)
 }
+
+## This is the old implementation. In case one need to backtrack to it.
+## Snapshot taken July 6, 2016
+## multiR.loglik <- function(data, chain, root, R){
+##     ## Default method is "rpf"
+##     C.m <- data$C.m
+##     X <- data$X
+##     D <- data$D
+##     n <- data$n
+##     r <- data$k
+##     p <- data$p
+##     V.m <- lapply(1:p, function(x) kronecker(R[[x]], C.m[[x]]) )
+##     V <- Reduce("+", V.m)
+##     ntot <- n*r
+##     ## This method will not accept matrices with elements equal to zero.
+##     ## Unclear if this would be enough to break the whole MCMC.
+##     ## Maybe check if elements are zero prior to calculating the loglik.
+##     ## But this might make the process slower. Maybe not slower than using always the
+##     ##     full inverse.
+##     ms <- 0 ## Here we do not allow for measurement error. Need to implement in the future.
+##     ## k <- r
+##     ## This is the relevant part. The calculation using C.
+##     error <- NULL
+##     cholres <- .Call(mvMORPH:::Chol_RPF, V, D, X, as.integer(r), as.integer(ntot), mserr=error, ismserr=as.integer(ms))
+##     det <- cholres[[2]]
+##     ## Adjusting the format of the data.
+##     Xvec <- as.vector(as.matrix(X))
+##     residus <- D %*% root - Xvec
+##     quad <- .Call(mvMORPH:::Chol_RPF_quadprod, cholres[[1]], residus, as.integer(ntot))
+##     logl <- -.5*quad-.5*as.numeric(det)-.5*(ntot*log(2*pi))
+##     return(logl)
+## }
