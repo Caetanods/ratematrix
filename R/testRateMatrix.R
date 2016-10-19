5cb0806417a42826d75bcd0e7ee95729e5de8b78
daniel <caetanods1@gmail.com>
2016-10-19 10:00:11 -0700

    Fix bug when matrix is 2x2

--- R/testRateMatrix.R
+++ R/testRateMatrix.R
@@ -4,50 +4,60 @@
 ##'    different. When the test is significant this means that the posterior distribution of the elements
 ##'    of the evolutionary rate matrices does not overlap more than 5%. This test is not a p value, since
 ##'    this is not an estimate of a probability of deviance from a given null distribution. On the other hand,
-##'    the test relies on the overlap of the posterior distribution. It assumes that when the posterior
+##'    the test relies on the median overlap of the posterior distribution. It assumes that when the posterior
 ##'    distribution of two or more paramaters do not overlap, then there is a significative difference
 ##'    between the parameters. Note that the test will applied to all pairwise combinations of the
-##'    matrices fitted to the tree.
+##'    matrices fitted to the tree. You can use the 'par' parameter to set which are the elements of the R matrix
+##'    you are interested in comparing.
 ##' @title Test different of posterior distributions of R matrices.
 ##' @param mcmc.chain The MCMC chain produced by the read function.
 ##' @param out The output of the MCMC chain. This is a list with a series of important informations about the MCMC chain.
-##' @param plot logical. Whether plots should be saved as .pdf files in the current directory. The number of the plot increases in function of the number of matrices fitted to the tree. All plots will have a pattern like "post_diff_mat_test_X.pdf", where X is a single digit number.
-##' @param file string. The name of the file to save the plot. Default is 'NULL' and will plot in the R standard plotting device. If a string is passed to it, then it will plot to a '.pdf' file. This is going to be used as a the start of the string. The number of plots is equal to the combination 2 by 2 of the number of the matrices fitted to the phylogentic tree.
+##' @param par This parameter sets the attribute of the R matrices that is checked by the test. Chose 'all' to test the median overlap among all cells of the covariance matrix. Chose 'rates' to check for the median overlap among the evolutionary rates of each trait (variance vector). Chose 'correlation' to test for the median overlap among the correlations (correlation matrix).
 ##' @return Return a matrix with the value of the test statistics. Values bellow 0.05 supports a difference between the posterior distribution of R matrices.
 ##' @export
-testRateMatrix <- function(mcmc.chain, out, plot = TRUE, file = NULL){
+testRateMatrix <- function(mcmc.chain, out, par="all"){
+    ## Right now the function offer for a plot. Better than that would be to print the partial results, the difference between each cell and then the median of the difference. Also return this as the elements of a list.
+    ## Right now the function is doing the tests but it is not returning the differences trait by trait. Need to find a way to do that.
     comb <- combn(1:out$p, 2)
-    mean.diff <- list()
-    for(i in ncol(comb)){
-        mat1 <- t( sapply(mcmc.chain$matrix[[comb[1,i]]], function(x) c(x) ) )
-        mat2 <- t( sapply(mcmc.chain$matrix[[comb[2,i]]], function(x) c(x) ) )
-        mat.diff <- mat1 - mat2
-        mean.diff[[i]] <- apply(mat.diff, 1, mean)
+    median.diff <- list()
+    if(par == "all"){
+        for(i in ncol(comb)){
+            mat1 <- t( sapply(mcmc.chain$matrix[[comb[1,i]]], function(x) c(x) ) )
+            mat2 <- t( sapply(mcmc.chain$matrix[[comb[2,i]]], function(x) c(x) ) )
+            mat.diff <- mat1 - mat2
+            median.diff[[i]] <- apply(mat.diff, 1, median)
+        }
     }
-    cdf.list <- lapply(mean.diff, FUN = ecdf)
-    qq.list <- lapply(cdf.list, FUN = function(x) x(0) )
-    test <- lapply(qq.list, FUN = function(x) 2*apply(cbind(x, 1-x), 1, min) )
-    if(plot == TRUE){
-        if( is.null(file) ){
-            hist(mean.diff[[i]], main = paste("mat #",comb[1,i]," vs. mat #",comb[2,i]," test value ="
-                                              , round(test[[i]], digits = 3))
-                   , freq = FALSE, xlab = "Posterior mean difference between matrices"
-               , col = "grey", border = "white")
-            abline(v = 0, col = "red", lty = 3, lwd = 3)
-            lines( density(mean.diff[[i]]) )
-        } else {
+    if(par == "rates"){
+        for(i in ncol(comb)){
+            mat1 <- t( sapply(mcmc.chain$matrix[[comb[1,i]]], function(x) c( diag(x) ) ) )
+            mat2 <- t( sapply(mcmc.chain$matrix[[comb[2,i]]], function(x) c( diag(x) ) ) )
+            mat.diff <- mat1 - mat2
+            median.diff[[i]] <- apply(mat.diff, 1, median)
+        }
+    }
+    if(par == "correlation"){
+        ## This part will break if the rate matrix is 2x2. The median do not need to be calculated.
+        if( ncol( mcmc.chain$matrix[[1]][[1]] ) > 2 ){
+            upper <- upper.tri( mcmc.chain$matrix[[1]][[1]] )
             for(i in ncol(comb)){
-                pdf( paste(file, i, ".pdf", sep="") )
-                hist(mean.diff[[i]], main = paste("mat #",comb[1,i]," vs. mat #",comb[2,i]," test value ="
-                                                , round(test[[i]], digits = 3))
-                   , freq = FALSE, xlab = "Posterior mean difference between matrices"
-                   , col = "grey", border = "white")
-                abline(v = 0, col = "red", lty = 3, lwd = 3)
-                lines( density(mean.diff[[i]]) )
-                dev.off()
+                mat1 <- t( sapply(mcmc.chain$matrix[[comb[1,i]]], function(x) c( cov2cor(x)[upper] ) ) )
+                mat2 <- t( sapply(mcmc.chain$matrix[[comb[2,i]]], function(x) c( cov2cor(x)[upper] ) ) )
+                mat.diff <- mat1 - mat2
+                median.diff[[i]] <- apply(mat.diff, 1, median)
+            }
+        } else{
+            upper <- upper.tri( mcmc.chain$matrix[[1]][[1]] )
+            for(i in ncol(comb)){
+                mat1 <- t( sapply(mcmc.chain$matrix[[comb[1,i]]], function(x) c( cov2cor(x)[upper] ) ) )
+                mat2 <- t( sapply(mcmc.chain$matrix[[comb[2,i]]], function(x) c( cov2cor(x)[upper] ) ) )
+                median.diff[[1]] <- mat1 - mat2 ## Not a real 'median', because there is only one correlation.
             }
         }
     }
+    cdf.list <- lapply(median.diff, FUN = ecdf)
+    qq.list <- lapply(cdf.list, FUN = function(x) x(0) )
+    test <- lapply(qq.list, FUN = function(x) 2*apply(cbind(x, 1-x), 1, min) )
     test.dt <- do.call(cbind, test)
     colnames(test.dt) <- paste("mat #",comb[1,], " x #", comb[2,], sep="")
     rownames(test.dt) <- "test value"
