eba1af0d10c6739c3633df3591ef5012c1b4fe5e
daniel <caetanods1@gmail.com>
2016-12-06 16:26:00 -0800

    Add main MCMC function. Both multiple and single regimes.

---
+++ R/ratematrixMCMC.R
@@ -0,0 +1,199 @@
+##' Function runs a MCMC chain to estimate the posterior distribution of the evolutionary rate matrix and the phylogenetic root value given a phylogeny and trait data. Prior distribution and starting state of the chain can be chosen from one of the options provided. The function also accepts custom priors and starting states. Note that function requires output files in the chosen directory to be created and accessed. See 'Details' and 'Examples' for more information.
+##'
+##' Talk about function 'x' to collapse two or more regimes of a 'simmap' class tree.
+##' Explain the structure of the prior so that people can build their own.
+##' Explain how to fix the possible problem of function cannot write to the current directory. Need to be something simple. Maybe bullet proff it somehow.\cr
+##' \cr
+##'Fuction creates files with the MCMC chain. Each run of the MCMC will be identified by a unique identifier to facilitate identification and prevent the function to overwrite results when running more than one MCMC chain in the same directory. See argument 'IDlen'. The files in the directory are: 'outname.ID.loglik': the log likelihood for each generation, 'outname.ID.n.matrix': the evolutionary rate matrix n, one per line. Function will create one file for each R matrix fitted to the tree, 'outname.ID.root': the root value, one per line. \cr
+##' \cr
+##' Additionally it returns a list object with information from the analysis to be used by other functions. This list is refered as the 'out' parameter in those functions. The list is composed by: 'acc_ratio' numeric vector with 0 when proposal is rejected and non-zero when proposals are accepted. 1 indicates that root value was accepted, 2 and higher indicates that the first or subsequent matrices were updated; 'run_time' in seconds; 'k' the number of matrices fitted to the tree; 'p' the number of traits in the analysis; 'ID' the identifier of the run; 'dir' directory were output files were saved; 'outname' the name of the chain, appended to the names of the files; 'trait.names' A vector of names of the traits in the same order as the rows of the R matrix, can be used as the argument 'leg' for the plotting function 'make.grid.plot'; 'data' the original data for the tips; 'phy' the phylogeny; 'prior' the list of prior functions; 'start' the list of starting parameters for the MCMC run; 'gen' the number of generations of the MCMC.
+##' @title Run the MCMC chain for the evolutionary rate matrix model.
+##' @param data a matrix with the data. Each column is a different trait and species names need to be provided as rownames (rownames(data) == phy$tip.label).
+##' @param phy a phylogeny of the class "simmap" with the mapped regimes. The number of evolutionary rate matrices fitted to the phylogeny is equal to the number of regimes in phy. Regime names will also be used. See 'Details'.
+##' @param prior the prior densities for the MCMC. Must be one of (i) "uniform", (ii) "empirical_mean" (the default), (iii) the output of the "makePriorSeparation" function or (iv) a list of functions. See 'Details'.
+##' @param start the starting state for the MCMC chain. Must be one of (i) "prior_sample" (the default), (ii) "mle", (iii) a list object. See 'Details'.
+##' @param gen number of generations for the chain.
+##' @param v value for the degrees of freedom parameter of the inverse-Wishart proposal distribution for the correlation matrix.
+##' @param w_sd value for the width of the uniform proposal distribution for the vector of standard deviations.
+##' @param w_mu value for the width of the uniform proposal distribution for the vector of root values (phylogenetic mean).
+##' @param prop the proposal frequencies for each parameter of the model (default is 'c(0.025,0.975)'). This needs to be a numeric vector of length 2. Each value need to be between 0 and 1 and the sum of the vector equal to be 1. These values are the probability that the phylogenetic mean or the set of evolutionary rate matrices will be updated at each step of the MCMC chain, respectively.
+##' @param chunk number of generations that the MCMC chain will be kept in the computer memory before writing to file. Larger values will use more RAM memory.
+##' @param dir path of the directory to write the files (default is 'NULL'). If 'NULL' then function will write files to current directory (check 'getwd()'). If directory does not exist, then function will create a new directory. If function does not have permission (or fail) to write files to the pointed directory, then program will stop with an error message.
+##' @param outname name for the MCMC chain (default is 'ratematrixMCMC'). Name will be used in all the files alongside a unique ID of numbers with length of 'IDlen'.
+##' @param IDlen length of digits of the numeric identifier used to name output files (default is 5).
+##' @param singlerate whether the function should fit a single regime and ignore the number of regimes painted to the tree (default is FALSE).
+##' @return Function returns a list with the details of the MCMC run and write the MCMC output to the directory in a series of files.
+##' @author daniel
+##' @export
+##' @importFrom mvMORPH mvBM
+##' @importFrom corpcor decompose.cov
+ratematrixMCMC <- function(data, phy, prior="empirical_mean", start="prior_sample", gen, v=50, w_sd=0.5, w_mu=0.5, prop=c(0.025,0.975), chunk=gen/100, dir=NULL, outname="ratematrixMCMC", IDlen=5, singlerate=FALSE){
+    ## Need to drop 'traitwise' and 'use_corr' arguments.
+
+    ## #######################
+    ## Block to check arguments, give warnings and etc.
+    if( class(data) == "data.frame" ) X <- as.matrix( X )
+
+    cat("\n")
+
+    ## Inform that default options are being used:
+    if( prior == "empirical_mean" ) cat("Using default prior. \n")
+    if( start == "prior_sample" ) cat("Using default starting point. \n")
+    if( v == 50 && w_sd == 0.5 && w_mu == 0.5 && prop[1] == 0.025 ) cat("Using default proposal settings. \n")
+    
+    if( !inherits(phy, what="simmap") ){
+        cat('phy is not of class "simmap". Fitting a sigle rate regime to the tree. \n')
+        no_phymap <- TRUE
+    } else{
+        no_phymap <- FALSE
+    }
+
+    ## Make a quick check down the road to see if the prior is working.
+    if( !inherits(prior, what="ratematrix_prior_function") ){
+        if( inherits(prior, what="character") ){
+            if( !prior %in% c("uniform","empirical_mean") ) stop("prior option was not recognized. Check details for the 'prior' argument.")
+        } else{
+            if( inherits(prior, what="list") ){
+                warning("MCMC chain using custom prior as speficied by argument 'prior'.")
+            } else{
+                stop("Custom prior need to be a list. Check function details.")
+            }
+        }
+    }
+
+    ## Make a quick check down the road to see if the start state is valid.
+    if( inherits(start, what="character") ){
+        if( !start %in% c("prior_sample","mle") ) stop("start state option was not recognized. Check details for the 'start' argument.")
+    } else{
+        if( inherits(start, what="list") || inherits(start, what="ratematrix_prior_sample")){
+            cat("MCMC chain using custom starting point as speficied by argument 'start'.\n")
+        } else{
+            stop("Custom start state need to be a list. Check function details.")
+        }
+    }
+
+    if( !class(gen) == "numeric" ) stop('gen need to be a "numeric" value.')
+    
+
+    ## #######################
+    ## Block to create the directory for the output:
+    if( is.null(dir) ){
+        dir <- "."
+        local <- getwd()
+        cat( paste("Output files saved to current working directory: ", local, "\n", sep="" ) )
+    } else{
+        dir.create(file.path(dir), showWarnings = FALSE) ## This line will not modify the previous directory, so great.
+        cat( paste("Output files saved to user defined directory: ", dir, "\n", sep="" ) )
+    }
+
+    ## #######################
+    ## Block to set the analysis. First division is whether one or more regimes are fitted to the tree.
+    if( no_phymap || singlerate ){
+        
+        r <- ncol( data )
+
+        ## #######################
+        ## Block to generate priors.
+        prior_run <- prior
+        if( inherits(prior, what="character") ){
+            if(prior == "uniform"){
+                max.data <- apply(data, 2, max)
+                min.data <- apply(data, 2, min)
+                mean.data <- colMeans(data)
+                lower.bound.data <- mean.data - ( (mean.data - min.data) * 10 )
+                higher.bound.data <- mean.data + ( (max.data - mean.data) * 10 )
+                par.mu <- cbind( lower.bound.data, higher.bound.data )
+                par.sd <- c(0,100)
+                prior_run <- makePriorSeparation(r=r, p=1, par.mu=par.mu, par.sd=par.sd )
+            }
+            if(prior == "empirical_mean"){
+                mn <- colMeans(data)
+                ssd <- apply(data, 2, sd)
+                par.mu <- as.matrix( cbind(mn, ssd) )
+                par.sd <- c(0,100)
+                prior_run <- makePriorSeparation(r=r, p=1, den.mu="norm", par.mu=par.mu, par.sd=par.sd)
+            }
+        }
+        
+        ## #######################
+        ## Block to generate start point.
+        start_run <- start
+        if( inherits(start, what="character") ){
+            if(start == "prior_sample"){
+                start_run <- samplePriorSeparation(n=1, prior=prior_run, sample.sd=FALSE)
+            }
+            if(start == "mle"){
+                mle.fit <- mvBM(tree=centrarchidae$phy.map, data=centrarchidae$data, model="BM1", method="pic")
+                decomp.R <- decompose.cov( mle.fit$sigma )
+                start_run <- list()
+                start$mu <- as.numeric( mle.fit$theta )
+                start$matrix <- unname( as.matrix( decomp.R$r ) )
+                start$sd <- as.numeric( sqrt(decomp.R$v) )
+            }
+        }
+        
+        out_single <- singleRegimeMCMC(X=data, phy=phy, start=start_run, prior=prior_run, gen=gen, v=v, w_sd=w_sd, w_mu=w_mu
+                                     , prop=prop, chunk=chunk, dir=dir, outname=outname, IDlen=IDlen, traitwise=FALSE)
+        return( out_single )
+        
+    } else{
+        
+        p <- ncol( phy$mapped.edge ) ## Multiple regimes.
+        r <- ncol( data )
+
+        ## #######################
+        ## Block to generate priors.
+        prior_run <- prior
+        if( inherits(prior, what="character") ){
+            if(prior == "uniform"){
+                max.data <- apply(data, 2, max)
+                min.data <- apply(data, 2, min)
+                mean.data <- colMeans(data)
+                lower.bound.data <- mean.data - ( (mean.data - min.data) * 10 )
+                higher.bound.data <- mean.data + ( (max.data - mean.data) * 10 )
+                par.mu <- cbind( lower.bound.data, higher.bound.data )
+                rep.sd.regime <- rep(c(0,100), times=p)
+                par.sd <- matrix(data=rep.sd.regime, nrow=p, ncol=2, byrow=TRUE)
+                prior_run <- makePriorSeparation(r=r, p=p, par.mu=par.mu, par.sd=par.sd )
+            }
+            if(prior == "empirical_mean"){
+                mn <- colMeans(data)
+                ssd <- apply(data, 2, sd)
+                par.mu <- as.matrix( cbind(mn, ssd) )
+                rep.sd.regime <- rep(c(0,100), times=p)
+                par.sd <- matrix(data=rep.sd.regime, nrow=p, ncol=2, byrow=TRUE)
+                prior_run <- makePriorSeparation(r=r, p=p, den.mu="norm", par.mu=par.mu, par.sd=par.sd)
+            }
+        }
+        
+        ## #######################
+        ## Block to generate start point.
+        start_run <- start
+        if( inherits(start, what="character") ){
+            if(start == "prior_sample"){
+                start_run <- samplePriorSeparation(n=1, prior=prior_run, sample.sd=FALSE)
+            }
+            if(start == "mle"){
+                mle.fit <- mvBM(tree=centrarchidae$phy.map, data=centrarchidae$data, model="BMM", method="rpf")
+                decomp.r <- list()
+                decomp.sd <- list()
+                for( i in 1:p ){
+                    decomp.R <- decompose.cov( mle.fit$sigma[,,i] )
+                    decomp.r[[i]] <- unname( as.matrix( decomp.R$r ) )
+                    decomp.sd[[i]] <- as.numeric( sqrt(decomp.R$v) )
+                }
+                start_run <- list()
+                start_run$mu <- as.numeric( mle.fit$theta )
+                start_run$matrix <- decomp.r
+                start_run$sd <- decomp.sd
+            }
+        }
+        
+        out_mult <- multRegimeMCMC(X=data, phy=phy, start=start_run, prior=prior_run, gen=gen, v=v, w_sd=w_sd, w_mu=w_mu
+                                 , prop=prop, chunk=chunk, dir=dir, outname=outname, IDlen=IDlen, traitwise=FALSE
+                                 , use_corr=FALSE)
+        return( out_mult )
+        
+    }
+    
+}
