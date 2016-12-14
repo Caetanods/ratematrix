ba596d23373288a5f3f3b3baea1f45d346dc7020
daniel <caetanods1@gmail.com>
2016-12-13 16:02:42 -0800

    Updates the read function for the new output format. In conflict with the single rate MCMC yet.

--- R/readSingleRegimeMCMC.R
+++ R/readSingleRegimeMCMC.R
@@ -10,46 +10,39 @@
 ##' @return List with the MCMC chain for the phylogenetic mean (root value) and evolutionary rate matrices (R). 'root' are the values for the phylogenetic mean; 'matrix' is a list with the chain of R matrices; 'log.lik' is the log likelihood (not posterior) for the chain.
 ##' @importFrom readr read_lines
 readSingleRegimeMCMC <- function(out, burn = 0.5, thin = 1, dir=NULL){
-    ## Function to read the files written by iwish fast.
-    ## Depends on the package 'readr'.
-    ## Function takes as arguments the output of 'singleR_iwish_fast'.
-    ## Optionally a different directory from the original can be provided.
-    ## Returns a list of root values and R matrices. Read for other functions.
-    ## In the future this function will read the whole output of the MCMC.
-    ## out = Output object from 'singleR_iwish_fast'.
-    ## burn = The percent of burn-in. Value between 0 and 1. The burn in will be taken out
-    ##       while the function read the output. This will save memory and speed up the process
-    ##       whitout throwing the data away.
-    ## thin = The thinning of the posterior. Same format as the argument 'by' in function 'seq'.
-    ## dir = Optional directory to find results. Different from out$dir.
-
+    
     if(is.null(dir)){
         direct <- out$dir
     } else{
         direct <- dir
     }
 
-    post <- seq(round(out$gen * burn), out$gen, by=thin)
+    post <- seq(round(out$gen * burn)+1, out$gen+1, by=thin) ## First line is the header.
 
-    lik <- read_lines( file=file.path(direct, paste(out$outname, ".", out$ID, ".loglik", sep="")) )
-    lik <- lik[post]
-    lik <- sapply(lik, function(x) as.numeric( strsplit(x=x, split=";", fixed=TRUE)[[1]] )
-                  , USE.NAMES=FALSE)
+    ## In this version the posterior is in a single file.
+    mcmc <- read_lines( file=file.path(direct, paste(out$outname, ".", out$ID, ".mcmc", sep="")) )
+    header <- mcmc[1]
+    header <- as.character( strsplit(x=header, split=";", fixed=TRUE)[[1]] )
+    mcmc <- mcmc[post]
+    mcmc <- sapply(mcmc, function(x) as.numeric( strsplit(x=x, split=";", fixed=TRUE)[[1]] )
+                 , USE.NAMES=FALSE)
     
-    RR <- read_lines( file=file.path(direct, paste(out$outname, ".", out$ID, ".matrix", sep="")) )
-    RR <- RR[post]
-    RR <- t( sapply(RR, function(x) as.numeric( strsplit(x=x, split=";", fixed=TRUE)[[1]] )
-                  , USE.NAMES=FALSE) )
-    MM <- lapply(1:dim(RR)[1], function(x) matrix( as.matrix(RR)[x,], nrow=out$k, dimnames=list(out$names,out$names) ) )
-    rm(RR) ## Save memory.
+    ## Define the columns correspondent to the matrix:
+    init <- 1
+    end <- out$k^2+1
+    RR <- lapply(1:dim(mcmc)[2], function(x) matrix( as.matrix(mcmc)[init:end,x], nrow=out$k ) )
+    ## names(RR) <- c("regime1","regime2") ## This need to be the name of the regimes!!
     
-    root <- read_lines( file=file.path(direct, paste(out$outname, ".", out$ID, ".root", sep="")) )
-    root <- root[post]
-    root <- t( sapply(root, function(x) as.numeric( strsplit(x=x, split=";", fixed=TRUE)[[1]] )
-                  , USE.NAMES=FALSE) )
-    colnames(root) <- out$names
+    ## Now find the root values.
+    root <- t(sapply(1:dim(mcmc)[2], function(x) as.numeric( as.matrix(mcmc)[(end+1):(end+out$k+1),x])))
+    colnames(root) <- out$names ## This need to be the names from the data matrix.
+    
+    ## The the loglik:
+    lik <- sapply(1:dim(mcmc)[2], function(x) as.matrix(mcmc)[dim(mcmc)[1],x])
 
-    out <- list(root = root, matrix = MM, log.lik = lik)
-    class(out) <- "ratematrix_single_chain"
+    out <- list(root = root, matrix = RR, log.lik = lik)
+    class(out) <- "ratematrix_multi_chain"
+    
     return( out )
+
 }
