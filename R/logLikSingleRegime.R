80839a883e8c62976fb14dc62fef8fab68ccd050
daniel <caetanods1@gmail.com>
2016-12-15 11:58:41 -0800

    Hides the function. Not to be used by the user.

--- R/logLikSingleRegime.R
+++ R/logLikSingleRegime.R
@@ -9,7 +9,6 @@
 ##' @return numeric. The log likelhood.
 ##' @references Revell, L. J., and D. C. Collar. 2009. Phylogenetic Analysis of the Evolutionary Correlation Using Likelihood. Evolution 63:1090–1100.
 ##' @references Ho, L. si T., and C. Ané. 2014. A Linear-Time Algorithm for Gaussian and Non-Gaussian Trait Evolution Models. Syst Biol 63:397–408.
-##' @export
 ##' @importFrom phylolm three.point.compute
 logLikSingleRegime <- function(data, chain, phy, root, R){
     ## X.c are the residuals. We can think of the root as the intercept of a linear regression.
