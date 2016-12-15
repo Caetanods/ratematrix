fed2a9e878bca4bb873d6fa6a28345fc85cda102
daniel <caetanods1@gmail.com>
2016-12-14 16:16:02 -0800

    Update output files.

--- R/writeToFile.R
+++ R/writeToFile.R
@@ -8,19 +8,21 @@
 ##' @return Return a updated version of cache.chain. This chain will now only have the last state in the chain. Function writes to file the content in 'cache.chain'. This function will append the new generations to the previous chunk.
 writeToFile <- function(files, cache.chain, chunk){
     ## Write cache.chain to files.
-    #ll <- length(cache.chain$chain)
     ll <- chunk+1
-    ## Never write the first element. Keep the last element.
-    sapply(2:ll, function(x) cat(cache.chain$lik[x],"\n",sep=";",file=files$log.lik, append=TRUE))
-    sapply(2:ll, function(x) cat(c(cache.chain$chain[[x]][[1]]),"\n",sep=";",file=files$root, append=TRUE))
-    sapply(2:ll, function(x) cat(c(cache.chain$chain[[x]][[4]]),"\n",sep=";",file=files$matrix, append=TRUE))
+
+    for( i in 2:ll){
+        cat( c(cache.chain$chain[[i]][[4]]), sep="; ", file=files[[1]], append=TRUE)
+        cat("; ", sep="", file=files[[1]], append=TRUE)
+        cat(cache.chain$chain[[i]][[1]], sep="; ", file=files[[1]], append=TRUE)
+        cat("; ", sep="", file=files[[1]], append=TRUE)        
+        cat(cache.chain$lik[i], sep="; ", file=files[[1]], append=TRUE)
+        cat("\n", file=files[[1]], append=TRUE)
+    }
 
     ## Keep the last element and clear the rest for memory saving.
     keep.chain <- cache.chain$chain[[ll]]
     keep.lik <- cache.chain$lik[ll]
-    ##cache.chain$chain <- list()
     cache.chain$chain <- vector(mode="list", length=chunk+1)
-    ##cache.chain$lik <- vector()
     cache.chain$lik <- vector(mode="numeric", length=chunk+1)
     cache.chain$chain[[1]] <- keep.chain
     cache.chain$lik[1] <- keep.lik
