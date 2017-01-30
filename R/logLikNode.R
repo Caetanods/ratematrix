897263f21f5dad3293a509f952f5ec05a24f4a3f
daniel <caetanods1@gmail.com>
2017-01-30 15:21:52 -0800

    Implementation comment

--- R/logLikNode.R
+++ R/logLikNode.R
@@ -2,7 +2,7 @@ logLikNode <- function(ss, sigma_len ,sigma_len_inv, k){
     ## Internal function to calculate the log likelihood at each node.
     ## ss = contrast.
     ## sigma_len = Sigma * branch length
-    ## sigma_len = solve( Sigma * branch length )
+    ## sigma_len_inv = solve( Sigma * branch length )
     ll <- -0.5 * ( k*log(2*pi) + log(det(sigma_len)) + (ss %*% sigma_len_inv %*% ss) )
     return(ll)
 }
