9cdab0ce01e687784051f35434f4ba1d6b78a9bb
daniel <caetanods1@gmail.com>
2017-12-04 10:44:40 -0600

    Cleans DESCRIPTION file and adds help for the package.

---
+++ R/ratematrix-package.R
@@ -0,0 +1,7 @@
+#' ratematrix.
+#'
+#' R package to estimate the evolutionary rate matrix using Bayesian Markov-chain Monte Carlo for multiple regimes.
+#'
+#' @name ratematrix
+#' @docType package
+NULL
