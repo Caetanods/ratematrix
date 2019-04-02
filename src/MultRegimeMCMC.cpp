1a8afa1df7259087a8293a1f8f45314137fc63bd
daniel <caetanods1@gmail.com>
2019-04-02 12:36:34 -0700

    Fix BUG with Jacobian at the starting state of the MCMC.

--- src/MultRegimeMCMC.cpp
+++ src/MultRegimeMCMC.cpp
@@ -980,7 +980,7 @@ std::string runRatematrixMCMC_C(arma::mat X, int k, int p, arma::vec nodes, arma
   for( int j=0; j < p; j++ ){
     for( int i=0; i < k; i++ ){
       // The jacobian is computed on the variances!
-      curr_jacobian[j] = curr_jacobian[j] + ( log( var_vec.col(j)[j] ) * log( (k-1.0)/2.0 ) );
+      curr_jacobian[j] = curr_jacobian[j] + ( log( var_vec(j,i) ) * log( (k-1.0)/2.0 ) );
     }
   }
 
@@ -1262,7 +1262,7 @@ std::string runRatematrixMultiMCMC_C(arma::mat X, int k, int p, arma::mat nodes,
   for( int j=0; j < p; j++ ){
     for( int i=0; i < k; i++ ){
       // The jacobian is computed on the variances!
-      curr_jacobian[j] = curr_jacobian[j] + ( log( var_vec.col(j)[j] ) * log( (k-1.0)/2.0 ) );
+      curr_jacobian[j] = curr_jacobian[j] + ( log( var_vec(j,i) ) * log( (k-1.0)/2.0 ) );
     }
   }
 
@@ -1742,7 +1742,7 @@ std::string runRatematrixMCMC_jointMk_C(arma::mat X, arma::mat datMk, int k, int
   for( int j=0; j < p; j++ ){
     for( int i=0; i < k; i++ ){
       // The jacobian is computed on the variances!
-      curr_jacobian[j] = curr_jacobian[j] + ( log( var_vec.col(j)[j] ) * log( (k-1.0)/2.0 ) );
+      curr_jacobian[j] = curr_jacobian[j] + ( log( var_vec(j,i) ) * log( (k-1.0)/2.0 ) );
     }
   }
 
